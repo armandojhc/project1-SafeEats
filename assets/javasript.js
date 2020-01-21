8d82097eeab51dc49f9e8dd5ffad3cd60cb56cd1
Matthew Sawyer <57121937+matthewleonardsawyer@users.noreply.github.com>
2020-01-21 17:48:57 -0500

    Finalized links and removed some javascript.

--- assets/javasript.js
+++ assets/javasript.js
@@ -96,26 +96,3 @@ function getGif(searchTerm) {
         $("#gifContainer").html(tmp);
     });
 }
-
-  $("button").on("click", function(event) {
-    event.preventDefault();
-    var x = $(this).data("search");
-    console.log(x);
-
-    var queryURL = "https://taco-randomizer.herokuapp.com/random/";
-
-    $.ajax({ url: queryURL, method: "GET" })
-        .done(function(reponse) {
-            console.log(response);
-
-        });
-  });
-
-  // TEMPORARY TEST
-  var queryURL = "https://taco-randomizer.herokuapp.com/random/";
-
-  $.ajax({ url: queryURL, method: "GET" })
-      .done(function(response) {
-          console.log(response);
-
-  });
