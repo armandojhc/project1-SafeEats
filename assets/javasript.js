0781739dc97c553c9cc672ef133f0cee56cb625e
Armando Herrera Canelon <armando.herrera91@gmail.com>
2020-01-20 23:38:09 -0500

    Added search button , drop down, drop down content, and event listener to the main search button

--- assets/javasript.js
+++ assets/javasript.js
@@ -1,19 +1,34 @@
-M.AutoInit();//This just initializes the modal, leave here please
+// M.AutoInit();//This just initializes the modal, leave here please
 
 $(document).ready(function() {
 
-  $("button").on("click", function(event) {
+// Search button Event Listener
+  M.AutoInit();
+$('select').formSelect();
+  $("#mainSearchButton").on("click", function(event) {
     event.preventDefault();
     var x = $(this).data("search");
     console.log(x);
+      
+      // Grabs Selected Values from drop down 
+        var instance = M.FormSelect.getInstance($('#healthExclusion'));
+        var healthExclusionList = instance.getSelectedValues();
+        console.log(healthExclusionList);
+        // Turns those values that are actually put on an array and with the .join method makes them one big string.
+        var healthExclusionString = healthExclusionList.join(",");
+        console.log(healthExclusionString);
 
-    var queryURL = "https://taco-randomizer.herokuapp.com/random/";
+      // Calling the searchRecipe function which makes the API call to extract the recipies based on the search criteria 
 
-    $.ajax({ url: queryURL, method: "GET" })
-        .done(function(reponse) {
-            console.log(response);
+        searchRecipe($('#recipeSearchBar').val() , healthExclusionString , $('#exludingSearch').val())
 
-        });
+    // var queryURL = "https://taco-randomizer.herokuapp.com/random/";
+
+    // // $.ajax({ url: queryURL, method: "GET" })
+    // //     .done(function(reponse) {
+    // //         console.log(response);
+
+    // //     });
   });
 
   // TEMPORARY TEST
