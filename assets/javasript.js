5d3053932db9f08b8721c49087c06d848c4bc1fb
Rojel Perez Vez <rojelpv@gmail.com>
2020-01-22 13:25:53 -0500

    fixing code lay out

--- assets/javasript.js
+++ assets/javasript.js
@@ -1,65 +1,65 @@
-M.AutoInit();//This just initializes the modal, leave here please
+M.AutoInit(); //This just initializes the modal, leave here please
 
 $(document).ready(function() {
-  //loadRecipes();
-  
-  tacoGenerator();
+    //loadRecipes();
 
-  $('select').formSelect();
+    tacoGenerator();
 
-  $("#mainSearchButton").on("click", function(event) {
-      event.preventDefault();
-      var x = $(this).data("search");
-      console.log(x);
-        
-      // Grabs Selected Values from drop down 
-      var instance = M.FormSelect.getInstance($('#healthExclusion'));
-      var healthExclusionList = instance.getSelectedValues();
-      console.log(healthExclusionList);
-      // Turns those values that are actually put on an array and with the .join method makes them one big string.
-      var healthExclusionString = healthExclusionList.join(",");
-      console.log(healthExclusionString);
-  
-      // Calling the searchRecipe function which makes the API call to extract the recipies based on the search criteria 
-  
-      searchRecipe($('#recipeSearchBar').val() , healthExclusionString , $('#exludingSearch').val());
-  
-  });
+    $('select').formSelect();
 
-  $("#recipeBtn").click(function() {
-    if($("#header").hasClass("greenSwitch")) {
-        savedPage();
+    $("#mainSearchButton").on("click", function(event) {
+        event.preventDefault();
+        var x = $(this).data("search");
+        console.log(x);
+
+        // Grabs Selected Values from drop down 
+        var instance = M.FormSelect.getInstance($('#healthExclusion'));
+        var healthExclusionList = instance.getSelectedValues();
+        console.log(healthExclusionList);
+        // Turns those values that are actually put on an array and with the .join method makes them one big string.
+        var healthExclusionString = healthExclusionList.join(",");
+        console.log(healthExclusionString);
+
+        // Calling the searchRecipe function which makes the API call to extract the recipies based on the search criteria 
+
+        searchRecipe($('#recipeSearchBar').val(), healthExclusionString, $('#exludingSearch').val());
+
+    });
+
+    $("#recipeBtn").click(function() {
+        if ($("#header").hasClass("greenSwitch")) {
+            savedPage();
+            //createList();
+        } else {
+            searchPage();
+        }
+        return;
+    });
+
+    $(document).on("click", ".view", function() {
+        //var recipe = $(this).attr('id');
+        //search() -- use what it returns
+        getGif("cat"); //instead of cat recipe var
+        $("#modalHeader").text("new header text");
+        $("#recipeContainer").text("new recipe content");
+        M.Modal.getInstance(recipeModal).open();
+        return;
+    });
+
+    $(document).on("click", ".add", function() {
+        //var recipe = $(this).attr('id');
+        $(this).css("visibility", "hidden");
+        //saveRecipes(this thingy)
+        return;
+    });
+
+    $(document).on("click", ".delete", function() {
+        //var recipe = $(this).attr('id');
+        //deleteRecipe(this thingy)
+        $("#main").empty();
         //createList();
-    } else {
-        searchPage();
-    }
-    return;
-  });
-  
-  $(document).on("click", ".view", function() {
-      //var recipe = $(this).attr('id');
-      //search() -- use what it returns
-      getGif("cat");//instead of cat recipe var
-      $("#modalHeader").text("new header text");
-      $("#recipeContainer").text("new recipe content");
-      M.Modal.getInstance(recipeModal).open();
-      return;
-  });
-
-  $(document).on("click", ".add", function() {
-      //var recipe = $(this).attr('id');
-      $(this).css("visibility", "hidden");
-      //saveRecipes(this thingy)
-      return;
-  });
-
-  $(document).on("click", ".delete", function() {
-      //var recipe = $(this).attr('id');
-      //deleteRecipe(this thingy)
-      $("#main").empty();
-      //createList();
-      return;
-  });
+        return;
+    });
 
 });
 
@@ -67,10 +67,10 @@ function searchPage() {
     $("#main").empty();
     $("#btnTxt").text("recipes");
     $(".md-24").text("menu_book");
-    if($("#header").hasClass("blueSwitch")){
+    if ($("#header").hasClass("blueSwitch")) {
         $("#header").toggleClass("blueSwitch");
     }
-    if($("#header").hasClass("yellowSwitch")){
+    if ($("#header").hasClass("yellowSwitch")) {
         $("#header").toggleClass("yellowSwitch");
     }
     $("#header").toggleClass("greenSwitch");
@@ -81,10 +81,10 @@ function resultPage() {
     $("#main").empty();
     $("#btnTxt").text("back");
     $(".md-24").text("keyboard_backspace");
-    if($("#header").hasClass("greenSwitch")){
+    if ($("#header").hasClass("greenSwitch")) {
         $("#header").toggleClass("greenSwitch");
     }
-    if($("#header").hasClass("yellowSwitch")){
+    if ($("#header").hasClass("yellowSwitch")) {
         $("#header").toggleClass("yellowSwitch");
     }
     $("#header").toggleClass("blueSwitch");
@@ -95,10 +95,10 @@ function savedPage() {
     $("#main").empty();
     $("#btnTxt").text("back");
     $(".md-24").text("keyboard_backspace");
-    if($("#header").hasClass("greenSwitch")){
+    if ($("#header").hasClass("greenSwitch")) {
         $("#header").toggleClass("greenSwitch");
     }
-    if($("#header").hasClass("blueSwitch")){
+    if ($("#header").hasClass("blueSwitch")) {
         $("#header").toggleClass("blueSwitch");
     }
     $("#header").toggleClass("yellowSwitch");
@@ -106,8 +106,8 @@ function savedPage() {
 }
 
 function getGif(searchTerm) {
-    
-    var linkUrl = "https://api.giphy.com/v1/gifs/search?q="+searchTerm+"&limit=1&api_key=aNaCVPYDVjQethaqMIIwbmIElaUThThF";
+
+    var linkUrl = "https://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&limit=1&api_key=aNaCVPYDVjQethaqMIIwbmIElaUThThF";
     var tmp = "<img id='gifImg' src='";
 
     $.ajax({
@@ -115,7 +115,7 @@ function getGif(searchTerm) {
         method: "GET"
     }).then(function(response) {
         var link = response.data[0].images.original.url;
-        tmp +=  link + "' alt='A gif of your selected recipe' >";
+        tmp += link + "' alt='A gif of your selected recipe' >";
         $("#gifContainer").html(tmp);
     });
 }
